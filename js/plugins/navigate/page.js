eed55ebb82d8b3e35e3b63e211d04181ff1a471a
Restyled.io <commits@restyled.io>
2024-06-04 20:05:41 +0000

    Restyled by clang-format

--- js/plugins/navigate/page.js
+++ js/plugins/navigate/page.js
@@ -1,184 +1,184 @@
 let oldBodyScriptTagHashes = []
 
-let attributesExemptFromScriptTagHashing = [
-    'data-csrf',
-    'aria-hidden',
-]
+    let attributesExemptFromScriptTagHashing =
+        [
+          'data-csrf',
+          'aria-hidden',
+        ]
 
-export function swapCurrentPageWithNewHtml(html, andThen) {
-    let newDocument = (new DOMParser()).parseFromString(html, "text/html")
-    let newBody = document.adoptNode(newDocument.body)
-    let newHead = document.adoptNode(newDocument.head)
+    export function swapCurrentPageWithNewHtml(html, andThen) {
+      let newDocument = (new DOMParser()).parseFromString(html, "text/html")
+      let newBody = document.adoptNode(newDocument.body)
+      let newHead = document.adoptNode(newDocument.head)
 
-    oldBodyScriptTagHashes = oldBodyScriptTagHashes.concat(Array.from(document.body.querySelectorAll('script')).map(i => {
-        return simpleHash(ignoreAttributes(i.outerHTML, attributesExemptFromScriptTagHashing))
-    }))
+      oldBodyScriptTagHashes = oldBodyScriptTagHashes.concat(
+          Array.from(document.body.querySelectorAll('script'))
+              .map(i => {return simpleHash(ignoreAttributes(
+                       i.outerHTML, attributesExemptFromScriptTagHashing))}))
 
-    let afterRemoteScriptsHaveLoaded = () => {}
+      let afterRemoteScriptsHaveLoaded =
+          () => {}
 
-    mergeNewHead(newHead).finally(() => {
-        afterRemoteScriptsHaveLoaded()
-    })
+                mergeNewHead(newHead)
+                    .finally(() => {afterRemoteScriptsHaveLoaded()})
 
-    prepNewBodyScriptTagsToRun(newBody, oldBodyScriptTagHashes)
+      prepNewBodyScriptTagsToRun(newBody, oldBodyScriptTagHashes)
 
-    let oldBody = document.body
+      let oldBody = document.body
 
-    document.body.replaceWith(newBody)
+      document.body.replaceWith(newBody)
 
-    Alpine.destroyTree(oldBody)
+      Alpine.destroyTree(oldBody)
 
-    andThen(i => afterRemoteScriptsHaveLoaded = i)
-}
+      andThen(i => afterRemoteScriptsHaveLoaded = i)
+    }
 
 function prepNewBodyScriptTagsToRun(newBody, oldBodyScriptTagHashes) {
-    newBody.querySelectorAll('script').forEach(i => {
-        // We don't want to re-run script tags marked as "data-navigate-once"...
-        if (i.hasAttribute('data-navigate-once')) {
-            // However, if they didn't exist on the previous page, we do.
-            // Therefore, we'll check the "old body script hashes" to
-            // see if it was already there before skipping it...
+  newBody.querySelectorAll('script').forEach(i => {
+    // We don't want to re-run script tags marked as "data-navigate-once"...
+    if (i.hasAttribute('data-navigate-once')) {
+      // However, if they didn't exist on the previous page, we do.
+      // Therefore, we'll check the "old body script hashes" to
+      // see if it was already there before skipping it...
 
-            let hash = simpleHash(
-                ignoreAttributes(i.outerHTML, attributesExemptFromScriptTagHashing)
-            )
+      let hash = simpleHash(
+          ignoreAttributes(i.outerHTML, attributesExemptFromScriptTagHashing))
 
-            if (oldBodyScriptTagHashes.includes(hash)) return
-        }
+      if (oldBodyScriptTagHashes.includes(hash)) return
+    }
 
-        i.replaceWith(cloneScriptTag(i))
-    })
+    i.replaceWith(cloneScriptTag(i))
+  })
 }
 
 function mergeNewHead(newHead) {
-    let children = Array.from(document.head.children)
-    let headChildrenHtmlLookup = children.map(i => i.outerHTML)
+  let children = Array.from(document.head.children)
+  let headChildrenHtmlLookup = children.map(i => i.outerHTML)
 
-    // Only add scripts and styles that aren't already loaded on the page.
-    let garbageCollector = document.createDocumentFragment()
+  // Only add scripts and styles that aren't already loaded on the page.
+  let garbageCollector = document.createDocumentFragment()
 
-    let touchedHeadElements = []
+  let touchedHeadElements = []
 
-    let remoteScriptsPromises = []
+      let remoteScriptsPromises = []
 
-    for (let child of Array.from(newHead.children)) {
-        if (isAsset(child)) {
-            if (! headChildrenHtmlLookup.includes(child.outerHTML)) {
-                if (isTracked(child)) {
-                    if (ifTheQueryStringChangedSinceLastRequest(child, children)) {
-                        setTimeout(() => window.location.reload())
-                    }
-                }
-
-                if (isScript(child)) {
-                    try {
-                        remoteScriptsPromises.push(
-                            injectScriptTagAndWaitForItToFullyLoad(
-                                cloneScriptTag(child)
-                            )
-                        )
-                    } catch (error) {
-                        // Let's eat any promise rejects so that we don't
-                        // break the rest of the Alpine intializing...
-                        // Any errors triggered by adding the script tag to the page
-                        // will still be thrown...
-                    }
-                } else {
-                    document.head.appendChild(child)
-                }
-            } else {
-                garbageCollector.appendChild(child)
-            }
-
-            touchedHeadElements.push(child)
+      for (let child of Array.from(newHead.children)) {
+    if (isAsset(child)) {
+      if (!headChildrenHtmlLookup.includes(child.outerHTML)) {
+        if (isTracked(child)) {
+          if (ifTheQueryStringChangedSinceLastRequest(child, children)) {
+            setTimeout(() => window.location.reload())
+          }
         }
+
+        if (isScript(child)) {
+          try {
+            remoteScriptsPromises.push(
+                injectScriptTagAndWaitForItToFullyLoad(cloneScriptTag(child)))
+          } catch (error) {
+            // Let's eat any promise rejects so that we don't
+            // break the rest of the Alpine intializing...
+            // Any errors triggered by adding the script tag to the page
+            // will still be thrown...
+          }
+        } else {
+          document.head.appendChild(child)
+        }
+      } else {
+        garbageCollector.appendChild(child)
+      }
+
+      touchedHeadElements.push(child)
     }
+  }
 
-    // Remove any assets that aren't on the new page...
-    // @todo: Re-enable this code and find a better way to managed injected stylesheets. See livewire/livewire#6824
-    // for (let child of Array.from(document.head.children)) {
-    //     if (isAsset(child)) {
-    //         if (! touchedHeadElements.some(i => i.outerHTML === child.outerHTML)) {
-    //             child.remove()
-    //         }
-    //     }
-    // }
+  // Remove any assets that aren't on the new page...
+  // @todo: Re-enable this code and find a better way to managed injected
+  // stylesheets. See livewire/livewire#6824 for (let child of
+  // Array.from(document.head.children)) {
+  //     if (isAsset(child)) {
+  //         if (! touchedHeadElements.some(i => i.outerHTML ===
+  //         child.outerHTML)) {
+  //             child.remove()
+  //         }
+  //     }
+  // }
 
-    // How to free up the garbage collector?
+  // How to free up the garbage collector?
 
-    // Remove existing non-asset elements like meta, base, title, template.
-    for (let child of Array.from(document.head.children)) {
-        if (! isAsset(child)) child.remove()
-    }
+  // Remove existing non-asset elements like meta, base, title, template.
+  for (let child of Array.from(document.head.children)) {
+    if (!isAsset(child))
+      child.remove()
+  }
 
-    // Add new non-asset elements left over in the new head element.
-    for (let child of Array.from(newHead.children)) {
-        document.head.appendChild(child)
-    }
+  // Add new non-asset elements left over in the new head element.
+  for (let child of Array.from(newHead.children)) {
+    document.head.appendChild(child)
+  }
 
-    return Promise.all(remoteScriptsPromises)
+  return Promise.all(remoteScriptsPromises)
 }
 
 async function injectScriptTagAndWaitForItToFullyLoad(script) {
-    return new Promise((resolve, reject) => {
-        // Script assets need to be loaded synchronously so that scripts have
-        // their global variables available...
-        if (script.src) {
-            script.onload = () => resolve()
-            script.onerror = () => reject()
-        } else {
-            resolve()
-        }
+  return new Promise((resolve, reject) => {
+    // Script assets need to be loaded synchronously so that scripts have
+    // their global variables available...
+    if (script.src) {
+      script.onload = () => resolve()
+      script.onerror = () => reject()
+    } else {
+      resolve()
+    }
 
-        document.head.appendChild(script)
-    })
+    document.head.appendChild(script)
+  })
 }
 
 function cloneScriptTag(el) {
-    let script = document.createElement('script')
+  let script = document.createElement('script')
 
-    script.textContent = el.textContent
-    script.async = el.async
+  script.textContent = el.textContent
+  script.async = el.async
 
-    for (let attr of el.attributes) {
-        script.setAttribute(attr.name, attr.value)
-    }
+                 for (let attr of el.attributes) {
+    script.setAttribute(attr.name, attr.value)
+  }
 
-    return script
+  return script
 }
 
-function isTracked(el) {
-    return el.hasAttribute('data-navigate-track')
-}
+function isTracked(el) { return el.hasAttribute('data-navigate-track') }
 
 function ifTheQueryStringChangedSinceLastRequest(el, currentHeadChildren) {
-    let [uri, queryString] = extractUriAndQueryString(el)
+  let [uri, queryString] = extractUriAndQueryString(el)
 
-    return currentHeadChildren.some(child => {
-        if (! isTracked(child)) return false
+  return currentHeadChildren.some(child => {
+    if (!isTracked(child))
+      return false
 
-        let [currentUri, currentQueryString] = extractUriAndQueryString(child)
+      let [currentUri, currentQueryString] = extractUriAndQueryString(child)
 
-        // Only consider a data-navigate-track element changed if the query string has changed (not the URI)...
-        if (currentUri === uri && queryString !== currentQueryString) return true
-    })
+      // Only consider a data-navigate-track element changed if the query string
+      // has changed (not the URI)...
+      if (currentUri === uri && queryString !== currentQueryString) return true
+  })
 }
 
 function extractUriAndQueryString(el) {
-    let url = isScript(el) ? el.src : el.href
+  let url = isScript(el) ? el.src : el.href
 
-    return url.split('?')
+  return url.split('?')
 }
 
 function isAsset(el) {
-    return (el.tagName.toLowerCase() === 'link' && el.getAttribute('rel').toLowerCase() === 'stylesheet')
-        || el.tagName.toLowerCase() === 'style'
-        || el.tagName.toLowerCase() === 'script'
+  return (el.tagName.toLowerCase() === 'link' &&
+          el.getAttribute('rel').toLowerCase() === 'stylesheet') ||
+         el.tagName.toLowerCase() === 'style' ||
+         el.tagName.toLowerCase() === 'script'
 }
 
-function isScript(el)   {
-    return el.tagName.toLowerCase() === 'script'
-}
+function isScript(el) { return el.tagName.toLowerCase() === 'script' }
 
 function simpleHash(str) {
     return str.split('').reduce((a, b) => {
@@ -189,7 +189,7 @@ function simpleHash(str) {
 }
 
 function ignoreAttributes(subject, attributesToRemove) {
-    let result = subject
+  let result = subject
 
     attributesToRemove.forEach(attr => {
         // Create a regex pattern to match the attribute and its value.
@@ -199,8 +199,8 @@ function ignoreAttributes(subject, attributesToRemove) {
         result = result.replace(regex, '')
     })
 
-    // Remove all whitespace to make things less flaky...
-    result = result.replaceAll(' ', '')
+        // Remove all whitespace to make things less flaky...
+        result = result.replaceAll(' ', '')
 
-    return result.trim()
+        return result.trim()
 }
